920cb15219d0526b4e44cd03a720e61a71399db3
Onn Khairuddin bin Rahmat <onn.khairuddin@gmail.com>
2018-03-11 18:59:43 +0800

    Initial import Attendance management.

---
+++ src/main/java/net/abyres/tm/otcsatt/OtcsAttApplication.java
@@ -0,0 +1,12 @@
+package net.abyres.tm.otcsatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtcsAttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtcsAttApplication.class, args);
+	}
+}
